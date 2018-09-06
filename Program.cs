282db4bbd7f25ad74594fa28e30a373db7d8c0dd
Tixeris <mrgagalis@gmail.com>
2018-09-06 12:31:35 +0300

    Code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle kvadratas1 = new Rectangle(5, 10, 0, 0);
+            Console.WriteLine("Plotas: " + kvadratas1.GetPlotas());
+            Console.WriteLine("Perimetras: " + kvadratas1.GetPerimeter());
+            Console.WriteLine("Istrizaine: " + kvadratas1.GetLine());
+            Console.WriteLine("Istrizaines vidurio taskas: " + "(" + kvadratas1.GetCoordinates() + ";" + kvadratas1.GetCoordinates1() + ")");
+            Console.ReadLine();
+        }
+    }
+
+}
+
---
+++ Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+internal class Rectangle
+    {
+        
+        private int x;
+        private int y;
+        private int ilgis;
+        private int plotis;
+        private double Istr;
+
+
+        public Rectangle(int c, int d, int x, int y)
+        {
+            ilgis = c;
+            plotis = d;
+            this.x = x;
+            this.y = y;
+        }
+    
+        public int GetPlotas()
+        {
+            return ilgis * plotis;
+        }
+
+        public int GetPerimeter()
+        {
+            return ilgis * 2 + plotis * 2;
+        }
+
+        public double GetLine()
+        {
+            Istr = Math.Sqrt(Math.Pow(ilgis, 2) + Math.Pow(plotis, 2));
+            Istr = Math.Round(Istr);
+            return Istr;
+        }
+
+
+        public double GetCoordinates()
+        {
+            double middlex;
+            middlex = plotis / 2;
+            return middlex;
+        }
+
+        public double GetCoordinates1()
+        {
+            double middley;
+            middley = ilgis / 2;
+            return middley;
+        }
+}
